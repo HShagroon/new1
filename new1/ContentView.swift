a4961a5ffb5494cf959d7a7a3ed7409c03177fcc
HShagroon <haneenshagroon@gmail.com>
2023-10-08 09:30:42 +0300

    Initial Commit

---
+++ new1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  new1
+//
+//  Created by Haneen Rida Shagroon on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ new1/new1App.swift
@@ -0,0 +1,17 @@
+//
+//  new1App.swift
+//  new1
+//
+//  Created by Haneen Rida Shagroon on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct new1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
